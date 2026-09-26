a22ee03e85b04a46a564534021e8ef8a94a30701
HalilB41 <halilbagmanci40@gmail.com>
2026-09-26 14:13:44 +0300

    guncelleme Cmt 26.09.2026 14:13:44,05

--- push.bat
+++ push.bat
@@ -1,5 +1,7 @@
 @echo off
 cd /d "%~dp0"
+rem OneDrive klasordeki dosyalari kilitledigi icin Git'in otomatik toparlama isini kapat
+git config gc.auto 0
 echo Degisiklikler GitHub'a gonderiliyor...
 git add .
 git commit -m "guncelleme %date% %time%"
